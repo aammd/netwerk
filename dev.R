ea3d8ef33b928638ce5e1a4c5b66a31b79223f6f
Andrew MacDonald <a.a.m.macdonald@gmail.com>
2021-05-06 13:31:23 -0400

    make github

--- dev.R
+++ dev.R
@@ -12,12 +12,22 @@ usethis::use_r("matrix_addition")
 devtools::document()
 devtools::install()
 
+
 ## load all the functions:
 
 devtools::load_all()
+
+
+# test the matrix function ------------------------------------------------
 
 usethis::use_r("matrix_sum")
 
 usethis::use_test("matrix_sum")
 
 devtools::test()
+
+
+# set up git and github ---------------------------------------------------
+
+usethis::use_git()
+
